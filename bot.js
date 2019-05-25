6ba0e9f369214687b70edea9dc36ab625a34cdea
Matej Drzik <matej.drzik@quanto.sk>
2019-05-25 21:38:18 +0200

    message sending, repo channel map

--- bot.js
+++ bot.js
@@ -1,13 +1,34 @@
 const Discord = require('discord.js')
+const settings = require('settings')
 
 module.exports = class Bot {
   constructor(token) {
     this.token = token
     this.client = new Discord.Client()
+    this.repoChannels = {}
+  }
+
+  async send(repository, msg) {
+    const channel = this.repoChannels[repository]
+    if (!channel) {
+      console.log(`skipping message for ${repository}, channel not found`)
+      return
+    }
+
+    client.channels.get(channel).send(msg)
+  }
+
+  initChannels() {
+    const repositories = Object.keys(settings.repo_channels)
+    repositories.forEach(repository => {
+      const channelName = settings.repo_channels[repository]
+      this.repoChannels[repository] = this.client.channels.find('name', channelName)
+    })
   }
 
   async connect() {
     await this.client.login(this.token)
+    await this.initChannels()
   }
 
 }
--- index.js
+++ index.js
@@ -3,14 +3,43 @@ const bodyParser = require('body-parser')
 const settings = require('./settings')
 const Bot = require('./bot')
 
+const bot = new Bot(settings.discord.token)
 const app = express()
 
 app.use(bodyParser.json())
 
-app.post('/hook', (req, res) => {
+app.post('/hook', async (req, res) => {
   console.log('hook', req.body)
+  const params = req.body
+  let msg = null
+  if (params.comment) {
+    msg = `[${params.repository.name}][${params.issue.title}] Comment ${params.action}:\n${params.comment.body}`
+  } else if (params.issue) {
+    msg = `[${params.repository.name}][${params.issue.title}] Issue ${params.action}:\n${params.issue.body}`
+  }
+
+  if (msg) {
+    try {
+      bot.send(msg)
+    } catch (e) {
+      console.error('bot error', e)
+    }
+  }
+
+  res.send({ success: true })
 })
 
-app.listen(settings.host, settings.port, () => {
+const initBot = async () => {
+  console.log('connecting bot...')
+  try {
+    await bot.connect()
+    console.log('GitMate Discord bot connected')
+  } catch (e) {
+    console.error('GitMate Discord bot connection error', e)
+  }
+}
+
+app.listen(settings.port, settings.host, async () => {
+  initBot()
   console.log(`GitMate running at http://${settings.host}:${settings.port}`)
 })
